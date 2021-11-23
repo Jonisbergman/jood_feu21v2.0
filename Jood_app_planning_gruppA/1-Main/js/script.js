fa838c812bcb73f9db1056ebf50c6e66e7f1d0f6
Jonis Bergman <jonis.bergman@utb.ecutbildning.se>
2021-11-23 11:24:25 +0100

    hej hej

--- Jood_app_planning_gruppA/1-Main/js/script.js
+++ Jood_app_planning_gruppA/1-Main/js/script.js
@@ -1,8 +1,15 @@
 var swiper = new Swiper(".mySwiper", {
   effect: "coverflow",
+  loop: true,
   grabCursor: true,
   centeredSlides: true,
   slidesPerView: "auto",
+  loopedSlides: "5",
+  // spaceBetween: "1",
+  // initialSlide: "3",
+  // setWrapperSize: true,
+
+  
   coverflowEffect: {
     rotate: 0,
     stretch: -80,
